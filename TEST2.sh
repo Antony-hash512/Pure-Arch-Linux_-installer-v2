ea2bb8266a367a4ad9088836b9e6fafd70b64c56
Mega Buldozer <antony.ictr@gmail.com>
2024-10-16 16:23:38 +0400

    фиксируем полученный результат перед тестированим

--- TEST2.sh
+++ TEST2.sh
@@ -10,7 +10,26 @@ else
 fi
 
 : <<'COMMENT'
-Тут будут добавлены примеры использования
+Примеры использования:
+declare -A new_point0=(
+    ["mount_point"]="/" 
+    ["type"]="new_subvol_in_btrfs_in_lvm" 
+    ["crypt_mode"]="pwd_in_none" 
+    ["name"]="@arch_system_test42_in_/dev/mainvg/gigabox_in_/dev/nvme0n1p8"
+)
+
+declare -A new_point1=(
+    ["mount_point"]="/home" 
+    ["type"]="new_subvol_in_btrfs_in_lvm" 
+    ["crypt_mode"]="key_in_none" 
+    ["name"]="@arch_openhome_in_/dev/mainvg/gigabox_in_/dev/nvme0n1p8"
+    ["keyfile"]=/etc/home.key
+)
+
+возможные значения type: format_ext4, new_subvol_in_btrfs, new_subvol_in_btrfs_in_lvm, new_ext4_in_lvm
+возможные значение crypt_mode: (для format_ext4, new_subvol_in_btrfs): none, file, pwd, (для new_subvol_in_btrfs_in_lvm, new_ext4_in_lvm): none_in_none, none_in_file, none_in_pwd, file_in_none, pwd_in_none: (случаи двойного шифорования не рассматриваем из-за избыточности такого действия), file или pwd - какой метод расшифровки будет использован при загрузке системы файл с ключём или пароль?
+keyfile: путь к файлу ключа (где создать или откуда использовать), требуется только при использовании опции с file
+name: название(я) тома(oв) и/или раздела (для вложенной структуры нужно использовать разделение "_in_" например: @arch_system42_in_/dev/mainvg/gigabox_in_/dev/nvme0n1p8)
 COMMENT
 
 # Создаём ассоциативные массивы для каждой строки "двумерного" массива
@@ -28,7 +47,6 @@ declare -A new_point1=(
     ["crypt_mode"]="none_in_none" 
     ["name"]="@arch_openhome_in_/dev/mainvg/gigabox_in_/dev/nvme0n1p8"
 )
-
 
 # Определяем количество массивов вида new_pointX автоматически
 ALL_NEW_POINTS=()
@@ -51,5 +69,45 @@ for row in "${ALL_NEW_POINTS[@]}"; do
     echo ""
 done
 
+i=0;
+for row in "${ALL_NEW_POINTS[@]}"; do
+    ((i++))
+    echo "$i. Проводим операции, связанные с точкой монтирования \"${current_row["mount_point"]}\" "
+    declare -n current_row="$row"  # Используем ссылку на ассоциативный массив по его имени
+    #case для различных значения ${current_row["type"]}
+    case "${current_row["type"]}" in
+        "format_ext4")
+            echo "Обработка типа: format_ext4"
+            # команды для обработки format_ext4
+            :
+            ;;
+        "new_subvol_in_btrfs")
+            echo "Обработка типа: new_subvol_in_btrfs"
+            # команды для обработки new_subvol_in_btrfs
+            :
+            ;;
+        "new_subvol_in_btrfs_in_lvm")
+            echo "Обработка типа: new_subvol_in_btrfs_in_lvm"
+            # Разбивка строки с разделителем "_in_" и запись значений в переменные
+            IFS='_in_' read -r -a parts <<< "${current_row["name"]}"
+            subvol_name="${parts[0]}"
+            lvm_path="${parts[1]}"
+            lv_path="${parts[2]}"
+            echo "Имя субтома Btrfs: $subvol_name"
+            echo "Путь к разделу LVM: $lvm_path"
+            echo "Логический том LVM: $lv_path"
+            ;;
+        "new_ext4_in_lvm")
+            echo "Обработка типа: new_ext4_in_lvm"
+            # команды для обработки new_ext4_in_lvm
+            :
+            ;;
+        *)
+            echo "Неизвестный тип: ${current_row["type"]}" >&2
+            exit 1
+            ;;
+    esac
+    echo ""
+done
 
 read -p "Нажмите Enter для выхода..."
