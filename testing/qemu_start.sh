2eda74c9e0f008d58d9e483cb7c7f1462e25436a
Mega Buldozer <antony.ictr@gmail.com>
2024-11-07 19:48:03 +0400

    aslo in the 2nd file

--- testing/qemu_start.sh
+++ testing/qemu_start.sh
@@ -6,7 +6,7 @@ qemu-system-x86_64 \
     -cpu host \
     -smp 2 \
     -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
-    -drive if=pflash,format=raw,file=/usr/share/edk2-ovmf/x64/OVMF_VARS.fd \
+    -drive if=pflash,format=raw,file=/home/mega/data/extra/vm_disks/OVMF_VARS.fd \
     -drive file=/home/mega/data/extra/vm_disks/archlinux_vm_disk.qcow2,format=qcow2 \
     -virtfs local,path=/home/mega/git/Pure-Arch-Linux_-installer-v2/,mount_tag=inst_scripts,security_model=none \
     -cdrom /home/mega/data/extra/torrents/archlinux-2024.11.01-x86_64.iso \
