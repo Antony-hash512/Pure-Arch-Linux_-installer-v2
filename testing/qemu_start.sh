d3d016340bfb97af51ca926923fbfe7cb0f18af5
Mega Buldozer <antony.ictr@gmail.com>
2024-11-11 23:22:13 +0400

    m

--- testing/qemu_start.sh
+++ testing/qemu_start.sh
@@ -9,7 +9,7 @@ qemu-system-x86_64 \
     -drive if=pflash,format=raw,file=/home/mega/data/extra/vm_disks/edk2/2024/OVMF_VARS.4m.fd \
     -drive file=/home/mega/data/extra/vm_disks/archlinux_vm_disk.qcow2,format=qcow2 \
     -virtfs local,path=/home/mega/git/Pure-Arch-Linux_-installer-v2/,mount_tag=inst_scripts,security_model=none \
-    -virtfs local,path=/home/mega/git/Pure-Arch-Linux_-installer-v2/cash-repo,mount_tag=pkgcache,security_model=none \
+    -virtfs local,path=/home/mega/git/Pure-Arch-Linux_-installer-v2/cash-repo/,mount_tag=pkgcache,security_model=none \
     -cdrom /home/mega/data/extra/torrents/archlinux-2024.11.01-x86_64.iso \
     -drive file=./mntfld.iso,media=cdrom \
     -boot order=d \
