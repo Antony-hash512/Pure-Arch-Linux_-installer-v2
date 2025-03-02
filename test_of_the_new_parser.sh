889a70e56635e6c4ad4bbc9c1d903d05c4560542
Antony <antony.ictr@gmail.com>
2025-03-02 18:14:16 +0400

    -the old code

--- test_of_the_new_parser.sh
+++ test_of_the_new_parser.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-: <<'COMMENT'
-ALL_DRIVERS_PACKS="$(python3 get_data_from_components_xml.py list_driverspacks)"
-ALL_SOFT_PACKS="$(python3 get_data_from_components_xml.py list_softpacks)"
-ALL_INSTALL_LOCATIONS="$(python3 get_data_from_components_xml.py list_install_locations)"
-ALL_SETTINGS_VARIANTS="$(python3 get_data_from_components_xml.py list_settings_variants)"
-
-echo "В файле components.xml найдены следующие драйверные паки:"
-for drv in $ALL_DRIVERS_PACKS; do
-    description=$(python3 get_data_from_components_xml.py driverspack "$drv" get_description)
-    echo "  $drv - $description"
-done
-
-echo -e "\nВ файле components.xml найдены следующие программные паки:"
-for soft in $ALL_SOFT_PACKS; do
-    description=$(python3 get_data_from_components_xml.py softpack "$soft" get_description)
-    echo "  $soft - $description" 
-done
-COMMENT
 
 # Определение переменных для цвета
 RED='\033[31m'
