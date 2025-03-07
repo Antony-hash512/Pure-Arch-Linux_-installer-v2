111e518f8e3777a2e421b3b3b520fb308134b47e
Antony <antony.ictr@gmail.com>
2025-03-08 00:35:30 +0400

    sound in vm is terned off

--- testing/qemu_start.sh
+++ testing/qemu_start.sh
@@ -17,7 +17,7 @@ qemu-system-x86_64 \
     -device virtio-net-pci,netdev=mynet0 \
     -display gtk,zoom-to-fit=off \
     -vga virtio -device virtio-vga,edid=on,xres=1280,yres=720 \
-    -audiodev pa,id=pa,server=unix:${XDG_RUNTIME_DIR}/pulse/native \
-    -device intel-hda \
-    -device hda-duplex,audiodev=pa
+    #-audiodev pa,id=pa,server=unix:${XDG_RUNTIME_DIR}/pulse/native \
+    #-device intel-hda \
+   # -device hda-duplex,audiodev=pa
 
