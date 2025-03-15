e818f1f6aaddc81345759b8a77d9b66a03f706af
Antony <antony.ictr@gmail.com>
2025-03-15 23:15:28 +0400

    developing of the testing script

--- parts_info_test.sh
+++ parts_info_test.sh
@@ -79,9 +79,78 @@ parse_xml() {
 
 }
 
+#создаём ассоциативный массив, который будет находить хотя бы одну точку монтирования по имени устройства btrfs
+declare -A ALL_BTRFS_MOUNTPOINTS
+#массив котырый будет содержать временные точки монтирования btrfs подлежащие размонтированию в конце работы скрипта
+declare -a SOME_BTRFS_MOUNTPOINTS_TO_UNMOUNT
+get_btrfs_mountpoint() {
+    local btrfs_device=$1 #требуется указать полный путь к устройству
+    local btrfs_mountpoint=""
+    # Определяем формат устройства btrfs
+    local btrfs_format=""
+    
+    # Проверяем формат устройства: /dev/name, /dev/mapper/vgname-name или /dev/vgname/name
+    if [[ "$btrfs_device" =~ ^/dev/[a-zA-Z0-9]+$ ]]; then
+        # Формат /dev/name (например, /dev/sda1)
+        btrfs_format="standard"
+    elif [[ "$btrfs_device" =~ ^/dev/mapper/[a-zA-Z0-9_]+-[a-zA-Z0-9_]+$ ]]; then
+        # Формат /dev/mapper/vgname-name (LVM через mapper)
+        btrfs_format="mapper"
+    elif [[ "$btrfs_device" =~ ^/dev/[a-zA-Z0-9_]+/[a-zA-Z0-9_]+$ ]]; then
+        # Формат /dev/vgname/name (LVM через vgname)
+        btrfs_format="vgpath"
+    else
+        # Неизвестный формат
+        echo -e "${RED}Устройство $btrfs_device имеет неизвестный формат${NC}" > &2
+        return
+    fi
+
+
+    
+    case "$btrfs_format" in
+        "standard"|"mapper")
+            btrfs_dev_basename=$(basename "$btrfs_device")
+            ;;
+        "vgpath")
+            #приводим к формату, который используется в lsblk
+            btrfs_dev_vgname=$(echo "$btrfs_device" | awk -F'/' '{print $3}')
+            btrfs_dev_lvname=$(echo "$btrfs_device" | awk -F'/' '{print $4}')
+            btrfs_dev_basename="$btrfs_dev_vgname-$btrfs_dev_lvname"
+            ;;
+    esac
+    
+    #TODO: реализовать парсинг lsblk
+    
+     
+    
+    
+    #проверяем, что устройство является btrfs
+
+    # if ! lsblk -l -o NAME,FSTYPE | grep "$btrfs_dev_basename" | awk '{print $2}' | grep -q "btrfs"; then
+        #echo -e "${RED}Устройство $btrfs_device не является btrfs${NC}" > &2
+        #return
+    #fi
+    ##проверяем, существует ли точка монтирования для данного устройства
+    #if ! lsblk -l -o NAME,MOUNTPOINTS | grep "$btrfs_dev_basename" | awk '{print $2}' | grep -q "/"; then
+        #echo -e "${RED}Точка монтирования для устройства $btrfs_device не найдена${NC}" > &2
+        #return
+    #fi
+    ##local btrfs_mountpoint=$(lsblk -o NAME,FSTYPE,SIZE,RM,RO,MOUNTPOINTS | grep "$btrfs_device" | awk '{print $6}')
+    #ALL_BTRFS_MOUNTPOINTS[$btrfs_mountpoint]=$btrfs_mountpoint
+}
+
+get_btrfs_subvolumes() {
+    local btrfs_device=$1
+    #получаем точку монтирования для устройства
+    local btrfs_mountpoint=$(get_btrfs_mountpoint "$btrfs_device")
+}
+
 
 echo -e "${YELLOW}список разделов до начала установки:${NC}"
 lsblk -o NAME,FSTYPE,SIZE,RM,RO,MOUNTPOINTS
+#lvs -o vg_name,lv_name,lv_size,lv_attr
+
+
 #создаём временный файл и сохраняем имя в переменную
 LSBLK_RAW_INFO="/tmp/lsblk_before_install.txt"
 touch $LSBLK_RAW_INFO
@@ -117,25 +186,38 @@ for row in "${NEW_MOUNTPOINTS[@]}"; do
     read -r -a names <<< "${name//_in_/ }"
     echo -e "${YELLOW}Точка монтирования $row:${NC} $mount_point $type $crypt_mode $name"
     echo "Тип монтирования: $type"
-
-    case "$type" in
-        "format_ext4")
-            :
+    case "$crypt_mode" in
+        "none"|"none_in_none")    
+        case "$type" in
+            "format_ext4")
+                :
+                ;;
+            "new_subvol_in_btrfs")
+                :
+                ;;
+            "new_subvol_in_btrfs_in_lvm")
+                :
+                ;;
+            "new_ext4_in_lvm")
+                :
+                ;;
+        esac
             ;;
-        "new_subvol_in_btrfs")
-            :
-            ;;
-        "new_subvol_in_btrfs_in_lvm")
-            :
-            ;;
-        "new_ext4_in_lvm")
-            :
+        *)
+            echo -e "${RED}Данный функционал пока не реализован: $crypt_mode${NC}"
             ;;
     esac
 done
 
 
 read -p "Нажмите Enter для продолжения"
+
+
+
+#размонтируем временные точки монтирования btrfs
+for mountpoint in "${SOME_BTRFS_MOUNTPOINTS_TO_UNMOUNT[@]}"; do
+    umount "$mountpoint"
+done
 
 exit 0
 
