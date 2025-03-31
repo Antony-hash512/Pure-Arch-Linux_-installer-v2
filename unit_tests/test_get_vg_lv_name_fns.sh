5f44b9465bf11615a3e6a1e7b447e355825d4420
Antony <antony.ictr@gmail.com>
2025-03-31 05:48:55 +0400

    test was fixed

--- unit_tests/test_get_vg_lv_name_fns.sh
+++ unit_tests/test_get_vg_lv_name_fns.sh
@@ -26,14 +26,19 @@ run_test() {
     
     if [[ "$result" == "$expected" ]]; then
         echo -e "${GREEN}УСПЕХ${NC}: $test_name"
+        echo "  Инпут: '$input'"
         echo "  Ожидалось: '$expected'"
         echo "  Получено:  '$result'"
     else
         echo -e "${RED}ОШИБКА${NC}: $test_name"
+        echo "  Инпут: '$input'"
         echo "  Ожидалось: '$expected'"
         echo "  Получено:  '$result'"
     fi
 }
+
+#при задании через mapper в basename должен быть один и только один одиночный дефис из идущих подряд
+#если там встречаются ещё дефисы, в остальных местах их количество подряд должно быть кратно двум
 
 # Тесты для стандартных форматов LVM устройств
 echo "Тестирование извлечения имен групп томов (VG):"
@@ -67,20 +72,20 @@ run_test "Формат vg/lv с плюсами - LV" "/dev/vg+name/lv+name" "lv+
 
 # Тесты для комбинированных специальных символов
 echo -e "\nТестирование извлечения имен из комбинированных форматов (VG):"
-run_test "Формат mapper с комбинированными символами - VG" "/dev/mapper/vg_name.test-lv-name+test" "vg_name.test" "vg"
+run_test "Формат mapper с комбинированными символами - VG" "/dev/mapper/vg_name.test-lv--name+test" "vg_name.test" "vg"
 run_test "Формат vg/lv с комбинированными символами - VG" "/dev/vg_name.test/lv-name+test" "vg_name.test" "vg"
 
 echo -e "\nТестирование извлечения имен из комбинированных форматов (LV):"
-run_test "Формат mapper с комбинированными символами - LV" "/dev/mapper/vg_name.test-lv-name+test" "lv-name+test" "lv"
+run_test "Формат mapper с комбинированными символами - LV" "/dev/mapper/vg_name.test-lv--name+test" "lv-name+test" "lv"
 run_test "Формат vg/lv с комбинированными символами - LV" "/dev/vg_name.test/lv-name+test" "lv-name+test" "lv"
 
 # Тесты для сложных случаев
 echo -e "\nТестирование сложных случаев (VG):"
-run_test "Множественные дефисы в VG и LV - VG" "/dev/mapper/vg-with-many-dashes-lv-with-many-dashes" "vg-with-many-dashes" "vg"
+run_test "Множественные дефисы в VG и LV - VG" "/dev/mapper/vg--with--many--dashes-lv--with--many--dashes" "vg-with-many-dashes" "vg"
 run_test "Множественные дефисы через путь VG - VG" "/dev/vg-with-many-dashes/lv-with-many-dashes" "vg-with-many-dashes" "vg"
 
 echo -e "\nТестирование сложных случаев (LV):"
-run_test "Множественные дефисы в VG и LV - LV" "/dev/mapper/vg-with-many-dashes-lv-with-many-dashes" "lv-with-many-dashes" "lv"
+run_test "Множественные дефисы в VG и LV - LV" "/dev/mapper/vg--with--many--dashes-lv--with--many--dashes" "lv-with-many-dashes" "lv"
 run_test "Множественные дефисы через путь VG - LV" "/dev/vg-with-many-dashes/lv-with-many-dashes" "lv-with-many-dashes" "lv"
 
 echo -e "\nВсе тесты выполнены." 
