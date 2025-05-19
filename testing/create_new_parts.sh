235026d682ed7833ccbcc5126a662fe2a3bcc4ff
Antony <antony.ictr@gmail.com>
2025-05-19 16:02:18 +0400

    update partion table

--- testing/create_new_parts.sh
+++ testing/create_new_parts.sh
@@ -10,6 +10,9 @@ parted -a optimal /dev/sda mkpart primary 20GiB 50GiB   # /dev/sda3 - для LVM
 parted -a optimal /dev/sda mkpart primary 50GiB 60GiB   # /dev/sda4 - btrfs 10 GB
 parted -a optimal /dev/sda mkpart primary 60GiB 70GiB   # /dev/sda5 - LUKS 10 GB
 parted -a optimal /dev/sda mkpart primary 70GiB 80GiB   # /dev/sda6 - ext4 10 GB
+parted -a optimal /dev/sda mkpart primary 80GiB 85GiB   # /dev/sda7 - 1й pv для lvm
+parted -a optimal /dev/sda mkpart primary 85GiB 90GiB  # /dev/sda8 - 2й pv для lvm
+
 
 # Установка флага загрузки для EFI раздела
 parted /dev/sda set 1 boot on
@@ -39,6 +42,20 @@ cryptsetup open /dev/opened_vg/luks_in_opened_lvm luks_inside_lvm
 cryptsetup luksFormat --align-payload=8192 /dev/sda5
 cryptsetup open /dev/sda5 luks_on_sda5
 
+# Создание LUKS на /dev/sda7 и /dev/sda8
+cryptsetup luksFormat --align-payload=8192 /dev/sda7
+cryptsetup luksFormat --align-payload=8192 /dev/sda8
+cryptsetup open /dev/sda7 luks_on_sda7
+cryptsetup open /dev/sda8 luks_on_sda8
+
+# Создание физических томов LVM на /dev/sda7 и /dev/sda8
+pvcreate --dataalignment 1m /dev/mapper/luks_on_sda7
+pvcreate --dataalignment 1m /dev/mapper/luks_on_sda8
+vgcreate double_locked_vg /dev/mapper/luks_on_sda7 /dev/mapper/luks_on_sda8
+lvcreate -n btrfs_in_double_locked_lvm -L 6G double_locked_vg
+
+
+
 # Форматирование разделов
 mkfs.fat -F32 /dev/sda1
 mkfs.btrfs -f /dev/locked_vg/btrfs_in_locked_lvm
@@ -47,6 +64,7 @@ mkfs.btrfs -f /dev/mapper/luks_inside_lvm
 mkfs.btrfs -f /dev/sda4
 mkfs.btrfs -f /dev/mapper/luks_on_sda5
 mkfs.ext4 /dev/sda6
+mkfs.btrfs -f /dev/double_locked_vg/btrfs_in_double_locked_lvm
 
 # Вывод информации о созданных разделах
 echo "Разделы созданы успешно."
