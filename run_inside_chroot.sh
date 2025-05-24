74c9107736b35e7cb450b71136d4478e2498ae3d
Antony <antony.ictr@gmail.com>
2025-05-24 14:53:04 +0400

    -task

--- run_inside_chroot.sh
+++ run_inside_chroot.sh
@@ -188,7 +188,7 @@ if [[ $ADD_FILES_TO_HOME = "true" ]]; then
     #распаковка tar-архива в домашнюю папку пользователя
     #испльзуем цикл для распаковки всех архивов
     for archive in $ARCHIVES_4HOME; do
-        tar --numeric-owner -xvf "/$archive" -C /home/$USERNAME
+        tar --numeric-owner --zstd -xvf "/$archive" -C /home/$USERNAME
         chown -R $MY_UID:$MY_UID /home/$USERNAME/
         rm "/$archive"
     done
