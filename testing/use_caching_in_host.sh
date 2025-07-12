a52807caedbace89f7a8c19664479ec302b4c71c
Antony <antony.ictr@gmail.com>
2025-07-12 22:34:35 +0400

    m

--- testing/use_caching_in_host.sh
+++ testing/use_caching_in_host.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 if ! pacman -Qi pacserve &>/dev/null; then
-    sudo pacman -S pacserve
+    echo "для кеширования уже скачанных пакетов необходимо установить пакет pacserve из aur"
+    read -p "Нажмите Enter для продолжения"
+    exit 0
 fi
 
-sudo systemctl start pacserve
+# Проверяем и запускаем pacserve, если он не запущен
+if ! systemctl is-active --quiet pacserve; then
+    echo "потребуется ввести пароль от root для запуска pacserve и pacserve-ports.service"
+    sudo systemctl start pacserve
+else
+    echo "Сервис pacserve уже запущен"
+fi
 
-sudo systemctl start pacserve-ports.service
+# Проверяем и запускаем pacserve-ports.service, если он не запущен
+if ! systemctl is-active --quiet pacserve-ports.service; then
+    sudo systemctl start pacserve-ports.service
+else
+    echo "Сервис pacserve-ports.service уже запущен"
+fi
