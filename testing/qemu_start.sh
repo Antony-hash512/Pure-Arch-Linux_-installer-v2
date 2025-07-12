3f54ff312bfa78866c3218c613e1e1526643e9e4
Antony <antony.ictr@gmail.com>
2025-07-12 21:58:36 +0400

    changes in testing  dir

--- testing/qemu_start.sh
+++ testing/qemu_start.sh
@@ -1,16 +1,67 @@
 #!/bin/bash
 
+CURRENT_MONTH=$(date +%m)
+CURRENT_YEAR=$(date +%Y)
+RAM_SIZE=4G
+CPU_CORES=4
+
+if [ ! -f [modified]OVMF_VARS.4m.fd ]; then
+    cp [original]OVMF_VARS.4m.fd [modified]OVMF_VARS.4m.fd
+fi
+
+if [ ! -f [modified]archlinux_vm_disk.qcow2 ]; then
+    cp [original]archlinux_vm_disk.qcow2 [modified]archlinux_vm_disk.qcow2
+fi
+
+if [ ! -f archlinux-${CURRENT_YEAR}.${CURRENT_MONTH}.01-x86_64.iso ]; then
+    if ! pacman -Qi aria2 &>/dev/null; then
+        sudo pacman -S aria2 --noconfirm
+    fi
+    if ! pacman -Qi wget &>/dev/null; then
+        sudo pacman -S wget --noconfirm
+    fi
+    if [ -f arch.torrent ]; then
+        rm arch.torrent
+    fi
+    wget https://archlinux.org/releng/releases/${CURRENT_YEAR}.${CURRENT_MONTH}.01/torrent/ -O arch.torrent
+    aria2c -i arch.torrent --enable-rpc=false --continue=true
+    if [ $? -eq 0 ]; then
+        echo "Скачивание завершено успешно!"
+        # Удаляем старые образы ISO (кроме текущего)
+        for iso_file in archlinux-*.iso; do
+            if [ -f "$iso_file" ] && [ "$iso_file" != "archlinux-${CURRENT_YEAR}.${CURRENT_MONTH}.01-x86_64.iso" ]; then
+                echo "Удаляем старый образ: $iso_file"
+                rm "$iso_file"
+            fi
+        done        
+        # Удаляем соответствующие .aria2 файлы
+        for aria2_file in archlinux-*.iso.aria2; do
+            if [ -f "$aria2_file" ] && [ "$aria2_file" != "archlinux-${CURRENT_YEAR}.${CURRENT_MONTH}.01-x86_64.iso.aria2" ]; then
+                echo "Удаляем файл aria2: $aria2_file"
+                rm "$aria2_file"
+            fi
+        done
+    else
+        echo "Произошла ошибка при скачивании."
+        exit 1
+    fi
+fi
+
+if ! pacman -Qi qemu-base &>/dev/null; then
+    sudo pacman -S qemu-base --noconfirm
+fi
+
 qemu-system-x86_64 \
     -enable-kvm \
-    -m 4G \
+    -m ${RAM_SIZE} \
     -cpu host \
-    -smp 4 \
-    -drive if=pflash,format=raw,readonly=on,file=/home/share/data/extra/vm_disks/edk2/2024/OVMF_CODE.4m.fd \
-    -drive if=pflash,format=raw,file=/home/share/data/extra/vm_disks/edk2/2024/OVMF_VARS.4m.fd \
-    -drive file=/home/share/data/extra/vm_disks/archlinux_vm_disk.qcow2,format=qcow2 \
-    -virtfs local,path=/home/share/git/Cryptful-Arch-Linux_-installer-v2/,mount_tag=inst_scripts,security_model=none \
-    -virtfs local,path=/home/share/git/Cryptful-Arch-Linux_-installer-v2/cash-repo/,mount_tag=pkgcache,security_model=none \
-    -cdrom /home/share/data/extra/torrents/archlinux-2025.07.01-x86_64.iso \
+    -smp ${CPU_CORES} \
+    -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.4m.fd \
+    -drive if=pflash,format=raw,file=[modified]OVMF_VARS.4m.fd \
+    -drive file=[modified]archlinux_vm_disk.qcow2,format=qcow2 \
+    -virtfs local,path=../,mount_tag=inst_scripts,security_model=none \
+    -virtfs local,path=../cache-repo/,mount_tag=pkgcache,security_model=none \
+    -cdrom archlinux-${CURRENT_YEAR}.${CURRENT_MONTH}.01-x86_64.iso \
     -drive file=./mntfld.iso,media=cdrom \
     -boot order=d \
     -netdev user,id=mynet0 \
