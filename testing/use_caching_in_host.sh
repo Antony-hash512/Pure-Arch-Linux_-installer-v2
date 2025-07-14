d096937fcbea074ea400ceb7ac5a72e949ecd39f
Antony <antony.ictr@gmail.com>
2025-07-15 00:32:47 +0400

    -task

--- testing/use_caching_in_host.sh
+++ testing/use_caching_in_host.sh
@@ -6,11 +6,33 @@ if ! pacman -Qi pacserve &>/dev/null; then
     exit 0
 fi
 
+#проверяем на права суперпользователя
+if [[ "$EUID" -ne 0 ]]; then
+    echo "СБОЙ: Этот скрипт должен быть запущен от имени суперпользователя (root)" >&2
+    exit 1
+fi
+
+
+cat /etc/pacserve/pacserve.service.conf
+echo 'должно быть так: PACSERVE_ARGS="--multicast --avahi --trust-pacserve-peers"'
+echo '1. обновить автоматически'
+echo '2. обновить вручную'
+echo '3. оставить как есть'
+read -p "Выберите вариант: " choice
+
+if [[ "$choice" == "1" ]]; then
+    echo 'PACSERVE_ARGS="--multicast --avahi --trust-pacserve-peers"' > /etc/pacserve/pacserve.service.conf
+elif [[ "$choice" == "2" ]]; then
+    read -p "Введите текстовый редактор [по умолчанию: nano]: " editor
+    editor=${editor:-nano}          # если ничего не ввели – возьмём nano
+    "$editor" /etc/pacserve/pacserve.service.conf
+else
+    echo "Сервис pacserve не будет обновлен"
+fi
+
 # Проверяем и запускаем pacserve, если он не запущен
 if ! systemctl is-active --quiet pacserve; then
-    echo "потребуется ввести пароль от root для запуска pacserve и pacserve-ports.service"
-    echo 'PACSERVE_ARGS="--multicast --avahi --trust-pacserve-peers"' | sudo tee /etc/pacserve/pacserve.service.conf
-    sudo systemctl start pacserve
+    systemctl start pacserve
 
 else
     echo "Сервис pacserve уже запущен"
@@ -18,9 +40,9 @@ fi
 
 # Проверяем и запускаем pacserve-ports.service, если он не запущен
 if ! systemctl is-active --quiet pacserve-ports.service; then
-    sudo systemctl start pacserve-ports.service
+    systemctl start pacserve-ports.service
 else
     echo "Сервис pacserve-ports.service уже запущен"
 fi
 
-echo "Для подключения к кешу пакетов в хосте, запустите скрипт INSTALL.sh с флагом --cache-qemu-pkgs"
+echo "Для подключения к кешу пакетов из хоста на qemu запустите скрипт INSTALL.sh с флагом --cache-qemu-pkgs"
